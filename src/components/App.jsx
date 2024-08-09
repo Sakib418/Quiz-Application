155dd1e0fc1a54ede9197f3d9c6df37613f73753
Sakib418 <ifthekerskb@gmail.com>
2024-08-10 01:05:26 +0600

    Part-5 Added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,13 @@ import "../styles/App.css";
 import Layout from "./Layout";
 import Home from "./Pages/Home";
 import Signup from "./Pages/Signup";
-
+import Login from "./Pages/Login";
 function App() {
   return (
     <Layout>
         {/* <Home /> */}
-        <Signup />
+        {/* <Signup /> */}
+        <Login />
     </Layout>
   );
 }
